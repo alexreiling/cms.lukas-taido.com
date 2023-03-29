1d89e2268ca169cdb8a815deed67850a2bcedbea
Alexander Reiling <alexanderreiling@googlemail.com>
2023-03-30 00:05:11 +0200

    Fixes

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,10 +7,9 @@ import Projects from "./collections/Projects";
 import Media from "./collections/Media";
 import Global from "./globals/Global";
 
-console.log("process.env.SERVER_URL", process.env.PAYLOAD_PUBLIC_SERVER_URL);
-
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || "http://localhost:3001",
+  // serverURL fucks up the build
+  // serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || "http://localhost:3001",
   admin: {
     user: Users.slug,
   },
