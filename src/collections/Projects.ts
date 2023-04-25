3aaea2241e9bac1f6e92bc8ce8526d02b6b7d405
Alexander Reiling <alexanderreiling@googlemail.com>
2023-04-26 01:51:29 +0200

    Make projects draftable [important: run migration script]

--- src/collections/Projects.ts
+++ src/collections/Projects.ts
@@ -7,8 +7,37 @@ const Projects: CollectionConfig = {
     useAsTitle: "title",
     defaultColumns: ["title", "date", "priority", "tags"],
   },
+  versions: {
+    drafts: {
+      autosave: true,
+    },
+  },
   access: {
-    read: () => true,
+    read: ({ req }) => {
+      // If there is a user logged in,
+      // let them retrieve all documents
+      if (req.user) return true;
+
+      // If there is no user,
+      // restrict the documents that are returned
+      // to only those where `_status` is equal to `published`
+      // or where `_status` does not exist
+      return {
+        or: [
+          {
+            _status: {
+              equals: "published",
+            },
+          },
+          {
+            _status: {
+              exists: false,
+            },
+          },
+        ],
+      };
+    },
+    admin: () => true,
   },
   fields: [
     {
@@ -34,14 +63,6 @@ const Projects: CollectionConfig = {
       type: "relationship",
       relationTo: "tags",
       hasMany: true,
-      admin: {
-        position: "sidebar",
-      },
-    },
-    {
-      name: "published",
-      type: "checkbox",
-      defaultValue: false,
       admin: {
         position: "sidebar",
       },
